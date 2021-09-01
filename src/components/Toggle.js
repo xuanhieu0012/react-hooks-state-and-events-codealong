26348f87681891c97cfe89766e9745086e9480a9
Hieu <xuanhieu0012@gmail.com>
2021-08-31 20:33:25 -0500

    Complete

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,19 @@
-import React from "react";
+import { Color } from "jest-matcher-utils/node_modules/chalk";
+import React, {useState} from "react";
+
+
+
+
 
 function Toggle() {
-  return <button>OFF</button>;
+  
+  let [isOn, setIsOn] = useState(false)
+  function handleClick(){
+    setIsOn(isOn => !isOn)
+
+  }
+  const color = isOn ? "red" :"white"
+  return <button style={{background: color}} onClick ={handleClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
